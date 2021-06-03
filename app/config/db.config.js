a292a2f59a892e6a97d7ae07994ee6e33835946a
deshanivanmali <deshanivanmali@gmail.com>
2021-06-02 22:46:10 -0700

    cinfig chaged

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "Dipal@0708",
+    USER: "admin",
+    PASSWORD: "admin",
     DB: "Segwitz-Assignment",
     dialect: "postgres",
     pool: {
