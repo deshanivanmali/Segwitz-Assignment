8aed966cd92ec63e7e77908e2a16bcc55beba47c
deshanivanmali <deshanivanmali@gmail.com>
2021-06-04 06:00:00 -0700

    data lungth bright

--- dbQuery.sql
+++ dbQuery.sql
@@ -3,7 +3,7 @@ CREATE DATABASE 'Segwitz-Assignment'
 CREATE TABLE "public"."sales" ( 
 	"id" Serial NOT NULL,
 	"userName" Character Varying( 50 ) NOT NULL,
-	"amount" Numeric( 5, 2 ) NOT NULL,
+	"amount" Numeric(7,2) NOT NULL,
 	"createdAt" Date NOT NULL,
 	"updatedAt" Date NOT NULL,
 	"cratedat" Date NOT NULL,
